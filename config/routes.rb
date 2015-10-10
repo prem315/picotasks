85f5d8692e29e53c23b35a269b33d33d1d013b44
Prem Patel <prem.patel3105@gmail.com>
2015-10-11 01:01:49 +0530

    add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'static_pages/home'
   get 'static_pages/contact'
   get 'static_pages/services'
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root "static_pages#home"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
